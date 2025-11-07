c7f793a7a0b8b194df2891bfe61ea95cfd2f268a
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-07 13:17:27 +0530

    Initial commit - DailyDrop Client Side

---
+++ Client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-white">
+      <h1 className="text-primary text-7xl font-semibold px-6 py-3 rounded-lg transition-colors">
+        DailyDrop
+      </h1>
+
+      <h1 className="text-primary-dull text-7xl font-semibold px-6 py-3 rounded-lg transition-colors">
+        App
+      </h1>
+    </div>
+  );
+};
+
+export default App;
