dd9503da671a4fade24286995d0858e496575cbf
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-08 03:04:07 +0530

    Created Title Component

---
+++ Client/src/components/Title.jsx
@@ -0,0 +1,16 @@
+// Client/src/components/Title.jsx
+import React from "react";
+
+const Title = ({ text1, text2 }) => {
+  return (
+    <div className="inline-flex gap-2 items-center mb-3">
+      <p className="text-primary-dull text-2xl md:text-3xl font-medium">
+        {text1} <span className="text-black font-medium">{text2}</span>
+      </p>
+
+      <p className="w-8 sm:w-12 h-px sm:h-0.5 bg-primary "></p>
+    </div>
+  );
+};
+
+export default Title;
