895b9760e4395071ee9877788bfd17718e0caa2c
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-09 03:08:52 +0530

    Setup MongoDB connection

--- Server/server.js
+++ Server/server.js
@@ -2,9 +2,13 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
+import connectDB from "./config/db.js";
+import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+await connectDB();
 
 /* -------- ALLOW MULTIPLE ORIGINS -------- */
 const allowedOrigins = ["http://localhost:5173"];
