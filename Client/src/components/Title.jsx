400d0b42f02e1e593682a28b575da75c0c925c21
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-08 22:21:02 +0530

    Updated Title Component

--- Client/src/components/Title.jsx
+++ Client/src/components/Title.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Title = ({ text1, text2 }) => {
   return (
-    <div className="inline-flex gap-2 items-center mb-3">
+    <div className="inline-flex gap-2 items-center mb-0">
       <p className="text-primary-dull text-2xl md:text-3xl font-medium">
         {text1} <span className="text-black font-medium">{text2}</span>
       </p>
