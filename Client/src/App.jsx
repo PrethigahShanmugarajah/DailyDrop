f3c430cb1ea20dd16a2186b34f6d9651724079ce
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-08 22:24:03 +0530

    Completed SellerLogin Component

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -13,6 +13,7 @@ import ProductDetails from "./pages/ProductDetails";
 import Cart from "./pages/Cart";
 import AddAddress from "./pages/AddAddress";
 import MyOrders from "./pages/MyOrders";
+import SellerLogin from "./components/seller/SellerLogin";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
@@ -54,6 +55,10 @@ const App = () => {
           <Route path="/cart" element={<Cart />} />
           <Route path="/add-address" element={<AddAddress />} />
           <Route path="/my-orders" element={<MyOrders />} />
+          <Route
+            path="/seller"
+            element={isSeller ? null : <SellerLogin />}
+          ></Route>
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
