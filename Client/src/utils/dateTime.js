c835466764d6d347b7a63f5716bbd283d53bb7b8
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-08 21:50:49 +0530

    Created utils

---
+++ Client/src/utils/dateTime.js
@@ -0,0 +1,16 @@
+// Client/src/utils/dateTime.js
+
+/* ---------------- Format date as DD/MM/YYYY HH:MM:SS AM/PM ---------------- */
+export const formatDate = (dateString) => {
+  if (!dateString) return "";
+  const date = new Date(dateString);
+  return date.toLocaleString("en-GB", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  });
+};
