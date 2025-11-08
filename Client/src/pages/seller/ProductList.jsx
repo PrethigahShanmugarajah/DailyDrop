cc652c3bceb18b6d8f48d3a48ae54095e9f61faa
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-09 00:19:38 +0530

    Completed ProductList Page

---
+++ Client/src/pages/seller/ProductList.jsx
@@ -0,0 +1,80 @@
+// Client/src/pages/seller/ProductList .jsx
+import React from "react";
+import Title from "../../components/Title";
+import { useAppContext } from "../../context/AppContext";
+
+const ProductList = () => {
+  const {
+    navigate,
+    user,
+    setUser,
+    isSeller,
+    setIsSeller,
+    showUserLogin,
+    setShowUserLogin,
+    products,
+    currency,
+    addToCart,
+    updateCartItem,
+    removeFromCart,
+    cartItems,
+    searchQuery,
+    setSearchQuery,
+    getCartCount,
+    getCartAmount,
+  } = useAppContext();
+
+  return (
+    <div className="no-scrollbar flex-1 h-[95vh] overflow-y-scroll flex -flex-col justify-between">
+      <div className="w-full md:p-10 p-4">
+        <Title text1={"All"} text2={"Products"} />
+        <div className="flex flex-col items-center max-w-4xl w-full overflow-hidden rounded-md bg-white border border-gray-200 mt-6">
+          <table className="md:table-auto table-fixed w-full overflow-hidden">
+            <thead className="text-black text-sm text-left">
+              <tr>
+                <th className="px-4 py-3 font-semibold truncate">Product</th>
+                <th className="px-4 py-3 font-semibold truncate">Category</th>
+                <th className="px-4 py-3 font-semibold truncate hidden md:block">
+                  Selling Price
+                </th>
+                <th className="px-4 py-3 font-semibold truncate">In Stock</th>
+              </tr>
+            </thead>
+
+            <tbody className="text-sm text-black">
+              {products.map((product) => (
+                <tr key={product._id} className="border-t border-gray-200">
+                  <td className="md:px-4 pl-2 md:pl-4 py-3 flex items-center space-x-3 truncate">
+                    <div className="border border-gray-200 rounded overflow-hidden">
+                      <img
+                        src={product.image[0]}
+                        alt="Product"
+                        className="w-16"
+                      />
+                    </div>
+                    <span className="truncate max-sm:hidden w-full">
+                      {product.name}
+                    </span>
+                  </td>
+                  <td className="px-4 py-3">{product.category}</td>
+                  <td className="px-4 py-3 max-sm:hidden">
+                    {currency} {product.offerPrice}
+                  </td>
+                  <td className="px-4 py-3">
+                    <label className="relative inline-flex items-center cursor-pointer text-gray-200 gap-3">
+                      <input type="checkbox" className="sr-only peer" />
+                      <div className="w-12 h-7 bg-slate-300 rounded-full peer peer-checked:bg-primary transition-colors duration-200"></div>
+                      <span className="dot absolute left-1 top-1 w-5 h-5 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-5"></span>
+                    </label>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
