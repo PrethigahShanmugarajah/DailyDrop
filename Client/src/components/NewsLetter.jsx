0c632ca5c436742bf8d3df3cb1fa31c5dcc9333e
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-08 11:10:26 +0530

    Completed NewsLetter Component

---
+++ Client/src/components/NewsLetter.jsx
@@ -0,0 +1,36 @@
+// Client/src/components/NewsLetter.jsx
+import React from "react";
+import Button from "./Button";
+
+const NewsLetter = () => {
+  return (
+    <div className="flex flex-col items-center justify-center text-center space-y-2 mt-24 pb-14">
+      <h1 className="md:text-4xl text-2xl font-semibold">Never Miss a Deal!</h1>
+      <p className="md:text-lg text-gray-500/70 pb-8">
+        Subscribe to get the latest offers, new arrivals, and exclusive
+        discounts
+      </p>
+
+      <form className="flex w-full max-w-2xl">
+        <div className="flex w-full border border-gray-300 rounded-md transition hover:rounded-full overflow-hidden">
+          <input
+            className="flex-1 px-3 py-2 text-gray-500 outline-none "
+            type="email"
+            placeholder="Enter your email id"
+            required
+          />
+
+          {/* <button
+            type="submit"
+            className="md:px-12 px-8 h-full text-white bg-primary hover:bg-primary-dull transition-all cursor-pointer rounded-md rounded-l-none"
+          >
+            Subscribe
+          </button> */}
+          <Button text="Subscribe" type="submit" variant="primary" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default NewsLetter;
--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import MainBanner from "../components/MainBanner";
 import Categories from "../components/Categories";
 import BestSeller from "../components/BestSeller";
 import BottomBanner from "../components/BottomBanner";
+import NewsLetter from "../components/NewsLetter";
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
       <Categories />
       <BestSeller />
       <BottomBanner />
+      <NewsLetter />
     </div>
   );
 };
