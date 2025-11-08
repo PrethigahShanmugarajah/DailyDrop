fc334b614bac76f7e1c07a59ef9abb0db18983da
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-08 10:40:22 +0530

    Updated Button Component

--- Client/src/components/Button.jsx
+++ Client/src/components/Button.jsx
@@ -1,15 +1,22 @@
 // Client/src/components/Button.jsx
 import React from "react";
 
-const Button = ({ text, onClick, className, type = "button" }) => {
-  const baseClasses =
-    "cursor-pointer px-8 py-2 bg-primary text-white rounded-md transition hover:bg-primary-dull hover:rounded-full  ";
+const Button = ({ text, onClick, className, type, variant }) => {
+  const variants = {
+    primary:
+      "bg-primary text-white border border-primary hover:bg-primary-dull",
+    secondary:
+      "bg-transparent text-black border border-black hover:bg-gray-100",
+    text: "bg-transparent text-black border-none hover:bg-transparent",
+  };
+
+  const baseClasses = `cursor-pointer px-8 py-2 rounded-md transition hover:rounded-full ${variants[variant]}`;
 
   return (
     <button
       type={type}
       onClick={onClick}
-      className={`${baseClasses} ${className ? className : ""}`}
+      className={`${baseClasses} ${className || ""}`}
     >
       {text}
     </button>
