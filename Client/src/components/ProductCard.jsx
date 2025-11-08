50d33574e00771e3e4fb69b248b93d6ce15a5ba9
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-08 17:45:18 +0530

    Updated ProductCard Component

--- Client/src/components/ProductCard.jsx
+++ Client/src/components/ProductCard.jsx
@@ -19,6 +19,10 @@ const ProductCard = ({ product }) => {
     updateCartItem,
     removeFromCart,
     cartItems,
+    searchQuery,
+    setSearchQuery,
+    getCartCount,
+    getCartAmount,
   } = useAppContext();
 
   return (
