034f8b8444e1b4bbffbc9a1a636b6ce33ef0a643
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-08 17:16:18 +0530

    Completed ProductDetails Page

---
+++ Client/src/pages/ProductDetails.jsx
@@ -0,0 +1,205 @@
+import React, { useEffect, useState } from "react";
+import { useAppContext } from "../context/AppContext";
+import { Link, useParams } from "react-router-dom";
+import { assets } from "../assets/assets";
+import { Star } from "lucide-react";
+import Button from "../components/Button";
+import Title from "../components/Title";
+import ProductCard from "../components/ProductCard";
+
+const ProductDetails = () => {
+  const {
+    navigate,
+    user,
+    setUser,
+    isSeller,
+    setIsSeller,
+    showUserLogin,
+    setShowUserLogin,
+    products,
+    currency,
+    addToCart,
+    updateCartItem,
+    removeFromCart,
+    cartItems,
+    searchQuery,
+    setSearchQuery,
+  } = useAppContext();
+  const { id } = useParams();
+  const [relatedProducts, setRelatedProducts] = useState([]);
+  const [thumbnail, setThumbnail] = useState(null);
+
+  const product = products.find((item) => item._id === id);
+
+  useEffect(() => {
+    if (products.length > 0) {
+      //   let productsCopy = product.slice();
+      //   productsCopy = productsCopy.filter(
+      //     (item) => product.category === item.category
+      //   );
+      //   setRelatedProducts(productsCopy.slice(0, 5));
+
+      let productsCopy = [...products];
+      productsCopy = productsCopy.filter(
+        (item) => item.category === product.category && item._id !== product._id
+      );
+      setRelatedProducts(productsCopy.slice(0, 5));
+    }
+  }, [products]);
+
+  useEffect(() => {
+    setThumbnail(product?.image[0] ? product.image[0] : null);
+  }, [product]);
+
+  return (
+    product && (
+      <div className="mt-12">
+        <p>
+          <Link to={"/"}>Home</Link> /<Link to={"/products"}> Products</Link> /
+          <Link
+            to={`/products/${product.category.toLowerCase()}/${product._id}`}
+          >
+            {" "}
+            {product.category}
+          </Link>{" "}
+          /<span className="text-primary"> {product.name}</span>
+        </p>
+
+        <div className="flex flex-col md:flex-row gap-16 mt-4">
+          <div className="flex gap-3">
+            <div className="flex flex-col gap-3">
+              {product.image.map((image, index) => (
+                <div
+                  key={index}
+                  onClick={() => setThumbnail(image)}
+                  className="border max-w-24 border-gray-500/30 rounded overflow-hidden cursor-pointer"
+                >
+                  <img src={image} alt={`Thumbnail ${index + 1}`} />
+                </div>
+              ))}
+            </div>
+
+            <div className="border border-gray-500/30 max-w-100 rounded overflow-hidden">
+              <img
+                src={thumbnail}
+                alt="Selected product"
+                className="w-full h-full object-cover"
+              />
+            </div>
+          </div>
+
+          <div className="text-sm w-full md:w-1/2">
+            <h1 className="text-3xl font-medium">{product.name}</h1>
+
+            <div className="flex items-center gap-0.5 mt-1">
+              {/* {Array(5)
+                .fill("")
+                .map((_, i) => (
+                  <img
+                    key={i}
+                    className="md:w-3.5 w-3.5"
+                    src={i < 4 ? assets.star_icon : assets.star_dull_icon}
+                    alt="Star"
+                  />
+                ))} */}
+
+              {Array(5)
+                .fill("")
+                .map((_, i) =>
+                  i < 4 ? (
+                    <Star
+                      key={i}
+                      size={15}
+                      className="text-primary fill-primary"
+                    />
+                  ) : (
+                    <Star key={i} size={15} className="text-peach fill-peach" />
+                  )
+                )}
+              <p className="text-base ml-2">(4)</p>
+            </div>
+
+            <div className="mt-6">
+              <p className="text-gray-500/70 line-through">
+                MRP: {currency} {product.price}
+              </p>
+
+              <p className="text-2xl font-medium">
+                MRP: {currency} {product.offerPrice}
+              </p>
+              <span className="text-gray-500/70">(inclusive of all taxes)</span>
+            </div>
+
+            <p className="text-base font-medium mt-6">About Product</p>
+            <ul className="list-disc ml-4 text-gray-500/70">
+              {product.description.map((desc, index) => (
+                <li key={index}>{desc}</li>
+              ))}
+            </ul>
+
+            <div className="flex items-center mt-10 gap-4 text-base">
+              {/* <button
+                onClick={() => addToCart(product._id)}
+                className="w-full py-3.5 cursor-pointer font-medium bg-gray-100 text-gray-800/80 hover:bg-gray-200 transition"
+              >
+                Add to Cart
+              </button> */}
+
+              <Button
+                text={"Add to Cart"}
+                onClick={() => addToCart(product._id)}
+                className="w-full"
+                variant="secondary"
+              />
+              {/* <button
+                onClick={() => {
+                  addToCart(product._id);
+                  navigate("/cart");
+                }}
+                className="w-full py-3.5 cursor-pointer font-medium bg-primary text-white hover:bg-primary-dull transition"
+              >
+                Buy now
+              </button> */}
+
+              <Button
+                text="Buy Now"
+                onClick={() => {
+                  addToCart(product._id);
+                  navigate("/cart");
+                }}
+                className="w-full"
+                variant="primary"
+              />
+            </div>
+          </div>
+        </div>
+        {/* ---------------- RELATED PRODUCTS ---------------- */}
+        <div className="flex flex-col items-center mt-20">
+          <div className="flex flex-col items-center w-max">
+            <Title text1={"Related"} text2={"Products"} />
+          </div>
+
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6 w-full">
+            {relatedProducts
+              .filter((product) => product.inStock)
+              .map((product, index) => (
+                <ProductCard key={index} product={product} />
+              ))}
+          </div>
+
+          <Button
+            text="See More"
+            onClick={() => {
+              navigate("/products");
+              scrollTo(0, 0);
+            }}
+            className="my-16"
+            variant="secondary"
+          />
+        </div>
+      </div>
+    )
+  );
+};
+
+export default ProductDetails;
