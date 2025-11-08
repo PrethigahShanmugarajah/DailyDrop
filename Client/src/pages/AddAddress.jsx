f56a44e16489d8309a0b6282d9e0dacaecc81a27
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-08 21:35:47 +0530

    Completed AddAddress Page

---
+++ Client/src/pages/AddAddress.jsx
@@ -0,0 +1,147 @@
+// Client/src/pages/AddAddress .jsx
+import React, { useState } from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/assets";
+import Button from "../components/Button";
+
+const InputField = ({ type, placeholder, name, handleChange, address }) => (
+  <input
+    className="w-full px-2 py-2.5 border border-gray-200 rounded outline-none text-black focus:border-primary transition placeholder-gray-300"
+    type={type}
+    placeholder={placeholder}
+    onChange={handleChange}
+    name={name}
+    value={address[name]}
+    required
+  />
+);
+
+const AddAddress = () => {
+  const [address, setAddress] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    country: "",
+    phone: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setAddress((prevAddress) => ({
+      ...prevAddress,
+      [name]: value,
+    }));
+  };
+
+  const onSubmitHandler = async (e) => {
+    e.preventFefault();
+  };
+
+  return (
+    <div className="mt-16 pb-16">
+      <Title text1={"Add Shipping"} text2={"Address"} />
+      <div className="flex md:flex-row justify-between mt-10">
+        <div className="flex-1 max-w-md">
+          <form onSubmit={onSubmitHandler} className="space-y-3 mt-6 text-sm">
+            <div className="grid grid-cols-2 gap-4">
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="firstName"
+                type="text"
+                placeholder="First Name"
+              />
+
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="lastName"
+                type="text"
+                placeholder="Last Name"
+              />
+            </div>
+
+            <InputField
+              handleChange={handleChange}
+              address={address}
+              name="email"
+              type="email"
+              placeholder="Email Address"
+            />
+
+            <InputField
+              handleChange={handleChange}
+              address={address}
+              name="street"
+              type="text"
+              placeholder="Street"
+            />
+
+            <div className="grid grid-cols-2 gap-4">
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="city"
+                type="text"
+                placeholder="City"
+              />
+
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="state"
+                type="text"
+                placeholder="State"
+              />
+            </div>
+
+            <div className="grid grid-cols-2 gap-4">
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="zipcode"
+                type="number"
+                placeholder="Zip Code"
+              />
+
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="country"
+                type="text"
+                placeholder="Country"
+              />
+            </div>
+
+            <InputField
+              handleChange={handleChange}
+              address={address}
+              name="phone"
+              type="number"
+              placeholder="Phone Number"
+            />
+
+            <Button
+              text={"Save Address"}
+              variant={"primary"}
+              className={"w-full"}
+            />
+          </form>
+        </div>
+
+        <img
+          className="md:mr-16 mb-16 md-mt-0 md:w-100 md:h-100 sm:w-50 sm:h-50"
+          src={assets.Add_Address_Image}
+          alt="Add Address"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AddAddress;
