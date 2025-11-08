8ba5e2debb8fa13e14d3f6a9792e8fa0224a04b5
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-09 00:40:14 +0530

    Completed Orders Page

---
+++ Client/src/pages/seller/Orders.jsx
@@ -0,0 +1,99 @@
+// Client/src/pages/seller/Orders .jsx
+import React, { useEffect, useState } from "react";
+import Title from "../../components/Title";
+import { useAppContext } from "../../context/AppContext";
+import { assets, dummyOrders } from "../../assets/assets";
+import { formatDate } from "../../utils/dateTime";
+
+const Orders = () => {
+  const {
+    navigate,
+    user,
+    setUser,
+    isSeller,
+    setIsSeller,
+    showUserLogin,
+    setShowUserLogin,
+    products,
+    currency,
+    addToCart,
+    updateCartItem,
+    removeFromCart,
+    cartItems,
+    searchQuery,
+    setSearchQuery,
+    getCartCount,
+    getCartAmount,
+  } = useAppContext();
+  const [orders, setOrders] = useState([]);
+
+  const fetchOrders = async () => {
+    setOrders(dummyOrders);
+  };
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
+  return (
+    <div className="no-scrollbar flex-1 h-[95vh] overflow-y-scroll">
+      <div className="md:p-10 p-4 space-y-4">
+        <Title text1={"Order"} text2={"List"} />
+        {orders.map((order, index) => (
+          <div
+            key={index}
+            className="flex flex-col md:items-center md:flex-row gap-5 justify-between p-5 max-w-4xl rounded-md border border-gray-300 mt-6"
+          >
+            <div className="flex gap-5 max-w-80">
+              <img
+                className="w-12 h-12 object-cover"
+                src={assets.Box_Icon}
+                alt="Box Icon"
+              />
+              <div>
+                {order.items.map((item, index) => (
+                  <div key={index} className="flex flex-col">
+                    <p className="font-medium text-black">
+                      {item.product.name}{" "}
+                      <span className="text-primary">x {item.quantity}</span>
+                    </p>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div className="text-sm md:text-base text-black/60">
+              <p className="text-black">
+                {order.address.firstName} {order.address.lastName}
+              </p>
+
+              <p>
+                {order.address.street}, {order.address.city}
+              </p>
+
+              <p>
+                {order.address.state},{order.address.zipcode},
+                {order.address.country}
+              </p>
+
+              <p></p>
+              <p>{order.address.phone}</p>
+            </div>
+
+            <p className="font-medium text-lg my-auto">
+              {currency} {order.amount}
+            </p>
+
+            <div className="flex flex-col text-sm">
+              <p>Method: {order.paymentType}</p>
+              <p>Date: {formatDate(order.createdAt)}</p>
+              <p>Payment: {order.isPaid ? "Paid" : "Pending"}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
