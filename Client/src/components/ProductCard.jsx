b6d68ea197df07466e2b0c84b85e7fbf7b90aaf4
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-08 21:51:43 +0530

    Updated ProductCard Component

--- Client/src/components/ProductCard.jsx
+++ Client/src/components/ProductCard.jsx
@@ -80,8 +80,7 @@ const ProductCard = ({ product }) => {
             <p className="md:text-xl text-base font-medium text-primary">
               {currency} {product.offerPrice}{" "}
               <span className="text-gray-500/60 md:text-sm text-xs line-through">
-                {currency}
-                {product.price}
+                {currency} {product.price}
               </span>
             </p>
             <div
