20923056d936d9670bc84f61e02d25b5d03a3823
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-08 17:43:43 +0530

    Updated BestSeller Component

--- Client/src/components/BestSeller.jsx
+++ Client/src/components/BestSeller.jsx
@@ -19,6 +19,10 @@ const BestSeller = () => {
     updateCartItem,
     removeFromCart,
     cartItems,
+    searchQuery,
+    setSearchQuery,
+    getCartCount,
+    getCartAmount,
   } = useAppContext();
 
   return (
