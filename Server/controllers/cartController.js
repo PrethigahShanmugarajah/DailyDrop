4a9c0505e4bc25054b89246ca6f4fa19e2e6846c
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-09 23:35:13 +0530

    Created Update User Cart Data Function

---
+++ Server/controllers/cartController.js
@@ -0,0 +1,51 @@
+// Server/controllers/cartController.js
+import User from "../models/User.js";
+
+/* -------- UPDATE USER CART DATA -------- */
+// export const updateCart = async (req, res) => {
+//   try {
+//     const { userId, cartItems } = req.body;
+//     await User.findByIdAndUpdate(userId, { cartItems });
+//     res.json({ success: true, message: "Cart Updated" });
+//   } catch (error) {
+//     console.error("Update User Cart Data Error:", error.message);
+
+//     return res.status(500).json({
+//       success: false,
+//       message: `Update User Cart Data Error: ${error.message}`,
+//     });
+//   }
+// };
+
+/* -------- UPDATE USER CART DATA -------- */
+export const updateCart = async (req, res) => {
+  try {
+    const { userId, cartItems } = req.body;
+
+    const user = await User.findByIdAndUpdate(
+      userId,
+      { cartItems },
+      { new: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Cart updated successfully",
+      cart: user.cartItems,
+    });
+  } catch (error) {
+    console.error("Update User Cart Data Error:", error.message);
+
+    return res.status(500).json({
+      success: false,
+      message: `Update User Cart Data Error: ${error.message}`,
+    });
+  }
+};
--- Server/server.js
+++ Server/server.js
@@ -8,6 +8,7 @@ import userRouter from "./routes/userRoute.js";
 import sellerRouter from "./routes/sellerRoute.js";
 import connectCloudinary from "./config/cloudinary.js";
 import productRouter from "./routes/productRoute.js";
+import cartRouter from "./routes/cartRoute.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -28,6 +29,7 @@ app.get("/", (req, res) => res.send("API is Working"));
 app.use("/api/user", userRouter);
 app.use("/api/seller", sellerRouter);
 app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
