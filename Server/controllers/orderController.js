c471c8a8170f4fe6c0fbb15daddb974d3e113bf0
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-10 00:54:22 +0530

    Created Get All Orders For Seller or Admin Function

--- Server/controllers/orderController.js
+++ Server/controllers/orderController.js
@@ -66,3 +66,23 @@ export const getUserOrders = async (req, res) => {
     });
   }
 };
+
+/* -------- GET ALL ORDERS FOR SELLER OR ADMIN -------- */
+export const getAllOrders = async (req, res) => {
+  try {
+    const orders = await Order.find({
+      $or: [{ paymentType: "COD" }, { isPaid: true }],
+    })
+      .populate("items.product address")
+      .sort({ createdAt: -1 });
+
+    return res.status(200).json({ success: true, orders });
+  } catch (error) {
+    console.error("Get All Orders For Seller or Admin Error:", error.message);
+
+    return res.status(500).json({
+      success: false,
+      message: `Get All Orders For Seller or Admin Error: ${error.message}`,
+    });
+  }
+};
