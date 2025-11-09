4f492949bfe490bae6456cc7114c0113d27c5f0d
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-10 00:45:22 +0530

    Created Get Orders By User[Id] Function

--- Server/controllers/orderController.js
+++ Server/controllers/orderController.js
@@ -44,3 +44,25 @@ export const placeOrderCOD = async (req, res) => {
     });
   }
 };
+
+/* -------- GET ORDERS BY USER[ID] -------- */
+export const getUserOrders = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const orders = await Order.find({
+      userId,
+      $or: [{ paymentType: "COD" }, { isPaid: true }],
+    })
+      .populate("items.product address")
+      .sort({ createdAt: -1 });
+
+    return res.status(200).json({ success: true, orders });
+  } catch (error) {
+    console.error("Get Orders By User[ID] Error:", error.message);
+
+    return res.status(500).json({
+      success: false,
+      message: `Get Orders By User[ID] Error: ${error.message}`,
+    });
+  }
+};
--- Server/routes/orderRoute.js
+++ Server/routes/orderRoute.js
@@ -1,10 +1,14 @@
 // Server/routes/orderRoute.js
 import express from "express";
 import { authUser } from "../middlewares/authUser.js";
-import { placeOrderCOD } from "../controllers/orderController.js";
+import {
+  getUserOrders,
+  placeOrderCOD,
+} from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/cod", authUser, placeOrderCOD);
+orderRouter.get("/user", authUser, getUserOrders);
 
 export default orderRouter;
