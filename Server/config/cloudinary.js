456484fe7551b21c12322ccffbf874f414c1e2da
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-09 21:55:26 +0530

    Fix Cloudinary configuration error

--- Server/config/cloudinary.js
+++ Server/config/cloudinary.js
@@ -1,12 +1,11 @@
 // Server/config/cloudinary.js
-import { v2 as cloudinary } from "cloudinary ";
+import { v2 as cloudinary } from "cloudinary";
 
 const connectCloudinary = async () => {
   cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env,
-    CLOUDINARY_API_SECRET,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
   });
 };
 
