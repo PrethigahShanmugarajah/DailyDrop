cd6b819f4727c37e0aa4d8b5e5e06710fcb248b7
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-10 00:02:11 +0530

    Created Get Address Function

--- Server/controllers/addressController.js
+++ Server/controllers/addressController.js
@@ -18,3 +18,19 @@ export const addAddress = async (req, res) => {
     });
   }
 };
+
+/* -------- GET ADDRESS -------- */
+export const getAddress = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const address = await Address.find({ userId });
+    return res.status(200).json({ success: true, address });
+  } catch (error) {
+    console.error("Get Address Error:", error.message);
+
+    return res.status(500).json({
+      success: false,
+      message: `Get Address Error: ${error.message}`,
+    });
+  }
+};
