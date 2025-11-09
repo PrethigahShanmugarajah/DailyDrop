64a49bc0ad3260db19082eb7ef28276e5e6c162e
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-09 20:58:21 +0530

    Created Seller Logout Function

--- Server/controllers/sellerController.js
+++ Server/controllers/sellerController.js
@@ -49,3 +49,26 @@ export const isSellerAuth = async (req, res) => {
     });
   }
 };
+
+/* -------- LOGOUT SELLER -------- */
+export const sellerLogout = async (req, res) => {
+  try {
+    res.clearCookie("sellerToken", {
+      httpOnly: true, // Prevent JavaScript to access cookie
+      secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+      samSite: process.env.NODE_ENV === "production" ? "none" : "strict", // CSRF protection
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Seller logged out successfully",
+    });
+  } catch (error) {
+    console.error("Logout Seller Error:", error.message);
+
+    return res.status(500).json({
+      success: false,
+      message: `Logout Seller Error: ${error.message}`,
+    });
+  }
+};
